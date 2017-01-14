7e5bc391444fe5887235bee58e57fb53a03c7cbd
arshad <arshdkhn1@gmail.com>
2017-01-13 20:00:00 -0600

    adding update

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Graph from './components/stock-graph/Graph.js';
 import Panel from './components/stock-panel/Panel.js';
+import 'bootstrap-grid';
 import './index.css';
 
 const App = () => {
   return (
     <div className="wrapper">
-      <Graph />
-      <Panel />
+      <Graph classes="col-xs-12"/>
+      <Panel classes=""/>
     </div>
   );
 }
