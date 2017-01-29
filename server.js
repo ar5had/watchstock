c615c4bd6a56384907fc6510dd2ab553581cad0f
arshad <arshdkhn1@gmail.com>
2017-01-30 00:23:15 +0530

    ...

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
