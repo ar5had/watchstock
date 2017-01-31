08f5b9fc1c1eaecdcf813e552d40efd0a2a886c6
arshad <arshdkhn1@gmail.com>
2017-02-01 02:30:08 +0530

    removed dot but

--- app/api.js
+++ app/api.js
@@ -1,10 +1,11 @@
 var rp = require('request-promise');
 var moment = require('moment');
-var qs = require('querystring');
 
 const getApiUrl = (symbol, date) => {
+  // replace all the dots in stock codes with underscore
+  symbol = symbol.replace(/\./g, "_");
 
-  return `https://www.quandl.com/api/v3/datasets/WIKI/${qs.escape(symbol)}\
+  return `https://www.quandl.com/api/v3/datasets/WIKI/${symbol}\
 .json?order=asc&column_index=4&start_date=${date}&api_key=${process.env.QKEY}`;
 };
 
